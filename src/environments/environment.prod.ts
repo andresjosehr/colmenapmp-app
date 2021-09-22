4beacbaba4b8dbd0e2776462535ba78ec4a7ff88
José Andrés <joseandreshernandezross@gmail.com>
2021-09-22 15:10:44 -0400

    Changes made

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.tusuperplan.cl/api',
-  assetsServerUrl: 'https://api.tusuperplan.cl'
+  apiUrl: 'http://colmenapmp.com/api/public/api',
+  assetsServerUrl: 'http://colmenapmp.com/api/public/api'
 };
