8acb5e75da9ec25a38e9e7735a8ef620b8f5fa30
José Andrés <joseandreshernandezross@gmail.com>
2021-09-22 15:53:13 -0400

    Changes made

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://colmenapmp.com/api/public/api',
-  assetsServerUrl: 'http://colmenapmp.com/api/public/api'
+  assetsServerUrl: 'http://colmenapmp.com/api/public'
 };
